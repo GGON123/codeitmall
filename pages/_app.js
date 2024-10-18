0c0c68affaa4278a8768ad739f4e3f145a867d6d
Kang Myoung Gon <jin04053@naver.com>
2024-10-18 16:46:13 +0900

    Drop initail code

---
+++ pages/_app.js
@@ -0,0 +1,34 @@
+import Head from "next/head";
+import Container from "@/components/Container";
+import Header from "@/components/Header";
+import { ThemeProvider } from "@/lib/ThemeContext";
+import "@/styles/global.css";
+
+import { Noto_Sans_KR } from "next/font/google";
+
+const notoSansKR = Noto_Sans_KR({
+  weight: ["400", "700"],
+  subsets: []
+});
+
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <title>Codeitmall</title>
+        <link rel="icon" href="/favicon.ico" />
+        <style>{`
+          html {
+            font-family: ${notoSansKR.style.fontFamily}, sans-serif;
+          }
+        `}</style>
+      </Head>
+      <ThemeProvider>
+        <Header />
+        <Container page>
+          <Component {...pageProps} />
+        </Container>
+      </ThemeProvider>
+    </>
+  );
+}
